30e2fbc77e23622f1b4a8204d9c9b02f70134e30
Drew Dalmedo <andrewdalmedo@gmail.com>
2024-11-09 15:55:44 -0500

    Add configs for more colorschemes

--- config/colorscheme.nix
+++ config/colorscheme.nix
@@ -1,11 +1,44 @@
 {
-  colorschemes.vscode = {
-    enable = true;
+  colorschemes = {
+    vscode = {
+      enable = false;
 
-    settings = {
-      italic_comments = true;
-      underline_links = true;
-      disable_nvimtree_bg = false;
+      settings = {
+        italic_comments = true;
+        underline_links = true;
+        disable_nvimtree_bg = false;
+      };
+    };
+
+    everforest = {
+      enable = false;
+
+      settings = {
+        background = "hard";
+        italic_comments = true;
+        underline_links = true;
+      };
+    };
+
+    poimandres = {
+      enable = false;
+    };
+
+    gruvbox = {
+      enable = false;
+    };
+
+    kanagawa = {
+      enable = false;
+
+      settings = {
+        terminalColors = true;
+        theme = "wave";
+      };
+    };
+
+    oxocarbon = {
+      enable = true;
     };
   };
 }
