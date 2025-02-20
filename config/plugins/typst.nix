2219974d6bcaa5a3845cd9d6bbf307e8155d14cc
Drew Dalmedo <andrewdalmedo@gmail.com>
2025-02-20 14:10:28 -0500

    Add keybind for live pdf preview

--- config/plugins/typst.nix
+++ config/plugins/typst.nix
@@ -2,4 +2,13 @@
   plugins.typst-vim = {
     enable = true;
   };
+
+  keymaps = [
+    # watch file for changes
+    {
+      mode = "n";
+      key = "<leader>cp";
+      action = "<cmd>TypstWatch<cr>";
+    }
+  ];
 }
