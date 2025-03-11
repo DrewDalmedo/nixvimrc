ae67b499bc950ff95a78f3314a25a3cefb365898
Drew Dalmedo <andrewdalmedo@gmail.com>
2025-03-11 12:55:17 -0400

    Add new line carry-over for bullet points

--- config/lua/glacier/ftbindings/typst.lua
+++ config/lua/glacier/ftbindings/typst.lua
@@ -1,5 +1,20 @@
 local M = {}
 
+local function starts_with_bullet(line)
+  -- Trim leading whitespace and check if line starts with -
+  return line:match("^%s*%-")
+end
+
+local function get_indent(line)
+  -- Get the indentation level of the line
+  return line:match("^(%s*)")
+end
+
+local function is_empty_bullet(line)
+  -- Check if line is just a bullet point with optional whitespace
+  return line:match("^%s*%-%s*$")
+end
+
 function M.setup()
   vim.api.nvim_create_autocmd('FileType', {
     pattern = 'typst',
@@ -9,6 +24,52 @@ function M.setup()
         silent = true,
         buffer = true,
         desc = "Add asterisks around word"
+      })
+
+      -- Handle Enter in insert mode
+      vim.keymap.set('i', '<CR>', function()
+        local line = vim.api.nvim_get_current_line()
+        if is_empty_bullet(line) then
+          -- Clear the current line and move to next line
+          return '<C-u><CR>'
+        elseif starts_with_bullet(line) then
+          local indent = get_indent(line)
+          return '<CR>' .. indent .. '- '
+        end
+        return '<CR>'
+      end, {
+        buffer = true,
+        expr = true,
+        desc = "Smart Enter for lists"
+      })
+
+      -- Handle o in normal mode
+      vim.keymap.set('n', 'o', function()
+        local line = vim.api.nvim_get_current_line()
+        if starts_with_bullet(line) then
+          local indent = get_indent(line)
+          return 'o' .. indent .. '- '
+        end
+        return 'o'
+      end, {
+        buffer = true,
+        expr = true,
+        desc = "Smart o for lists"
+      })
+
+      -- Handle O in normal mode
+      vim.keymap.set('n', 'O', function()
+        local curr_line_nr = vim.api.nvim_win_get_cursor(0)[1]
+        local prev_line = vim.api.nvim_buf_get_lines(0, curr_line_nr - 2, curr_line_nr - 1, false)[1]
+        if prev_line and starts_with_bullet(prev_line) then
+          local indent = get_indent(prev_line)
+          return 'O' .. indent .. '- '
+        end
+        return 'O'
+      end, {
+        buffer = true,
+        expr = true,
+        desc = "Smart O for lists"
       })
     end,
   })
