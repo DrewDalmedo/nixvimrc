710abbabc962f429b906864a3fc13e0fde3287cf
Drew Dalmedo <andrewdalmedo@gmail.com>
2025-03-11 13:29:20 -0400

    Indent whole item with tab in insert mode

--- config/lua/glacier/ftbindings/typst.lua
+++ config/lua/glacier/ftbindings/typst.lua
@@ -1,5 +1,15 @@
 local M = {}
 
+local function get_indent(line)
+  -- Get the leading whitespace of the line
+  local indent = line:match("^(%s*)")
+  -- Return empty string if there's no indent or just one space
+  if #indent <= 1 then
+    return ""
+  end
+  return indent
+end
+
 local function get_bullet_type(line)
   -- Return the bullet type (- or +) if present, nil otherwise
   local bullet = line:match("^%s*([%-+])")
@@ -13,7 +23,8 @@ end
 
 local function is_empty_bullet(line)
   -- Check if line is just a bullet point with optional whitespace
-  return line:match("^%s*[%-+]%s*$")
+  -- Must have a bullet point (- or +) to match
+  return line:match("^%s*[%-+]%s*$") ~= nil and starts_with_bullet(line)
 end
 
 function M.setup()
@@ -34,8 +45,9 @@ function M.setup()
           -- Clear the current line and move to next line
           return '<C-u><CR>'
         elseif starts_with_bullet(line) then
+          local indent = get_indent(line)
           local bullet = get_bullet_type(line) or '-'
-          return '<CR>' .. bullet .. ' '
+          return '<CR>' .. indent .. bullet .. ' '
         end
         return '<CR>'
       end, {
@@ -48,8 +60,9 @@ function M.setup()
       vim.keymap.set('n', 'o', function()
         local line = vim.api.nvim_get_current_line()
         if starts_with_bullet(line) then
+          local indent = get_indent(line)
           local bullet = get_bullet_type(line) or '-'
-          return 'o' .. bullet .. ' '
+          return 'o' .. indent .. bullet .. ' '
         end
         return 'o'
       end, {
@@ -58,13 +71,28 @@ function M.setup()
         desc = "Smart o for lists"
       })
 
+      -- Handle Tab in insert mode for bullet indentation
+      vim.keymap.set('i', '<Tab>', function()
+        local line = vim.api.nvim_get_current_line()
+        if is_empty_bullet(line) then
+          -- Add two spaces at the start of the line and keep cursor at end
+          return '<Esc>I  <End>'
+        end
+        return '<Tab>'
+      end, {
+        buffer = true,
+        expr = true,
+        desc = "Smart Tab for bullet indentation"
+      })
+
       -- Handle O in normal mode
       vim.keymap.set('n', 'O', function()
         local curr_line_nr = vim.api.nvim_win_get_cursor(0)[1]
         local prev_line = vim.api.nvim_buf_get_lines(0, curr_line_nr - 2, curr_line_nr - 1, false)[1]
         if prev_line and starts_with_bullet(prev_line) then
+          local indent = get_indent(prev_line)
           local bullet = get_bullet_type(prev_line) or '-'
-          return 'O' .. bullet .. ' '
+          return 'O' .. indent .. bullet .. ' '
         end
         return 'O'
       end, {
